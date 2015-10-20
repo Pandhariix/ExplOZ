2efdd1c311fdc41c59706bcbe1980b3f6b4a4c8f
Romain BOUCHER <Romain BOUCHER>
2015-10-20 22:17:43 +0200

    ajout region.cpp

---
+++ gitTest/region.cpp
@@ -0,0 +1,21 @@
+#include "capitale.h"
+
+Capitale::Capitale() : Ville()
+{
+    this->gouvernement = "none";
+}
+
+Capitale::Capitale(int nbHabitants, double superficie, std::string name, std::string gouvernement) : Ville(nbHabitants, superficie, name)
+{
+    this->gouvernement = gouvernement;
+}
+
+std::string Capitale::getGouvernement()
+{
+    return this->gouvernement;
+}
+
+std::string Capitale::getBolosse()
+{
+    return this->gouvernement;
+}
