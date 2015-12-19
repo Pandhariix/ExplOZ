a2c74e6f033174cc77817a66c8380d8877c297ef
PierreTh <pierre.thacoor@gmail.com>
2015-12-19 16:15:06 +0100

    Edition de la classe Receiver

--- ExplOZ_IA/receiver.cpp
+++ ExplOZ_IA/receiver.cpp
@@ -6,11 +6,11 @@ Receiver::Receiver(QString ip, quint16 port) : Communicator(ip, port)
 }
 
 void Receiver::start(){
-    socket.connectToHost(ip,port);
+    socket.connectToHost(ip, port);
 
     connect(&socket, SIGNAL(readyRead()), this, SLOT(extract()));
 }
 
 void Receiver::extract(){
-
+    //TODO
 }
